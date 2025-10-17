30f2cef808da4a7d605f8ea7dceb79e573709931
GIEZYMTZ <giezy3d@gmail.com>
2025-10-17 15:10:24 -0700

    Se crea e inicializa la base de datos sino se ha creado. Se registra el contexto de base de datos y se consultan los datos desde el componente blazor

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
+using BlazingPizza.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+// Register the database context with SQLite using AddSqlite
+builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
+
+// Register a service for HTTP commands
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
@@ -13,8 +23,20 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 app.UseRouting();
 
+
+
 app.MapRazorPages();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 
+// Initialize the database context using IServiceScopeFactory
+using (var scope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+    if (dbContext.Database.EnsureCreated())
+    {
+        SeedData.Initialize(dbContext);
+    }
+}
 app.Run();
